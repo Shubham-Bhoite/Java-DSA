336f46c85b7ef0428afe3e67b2f1244d13f229d4
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-08-24 22:38:04 +0530

    DAY 6

---
+++ Functions/BasicSum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class BasicSum {
+    public static void CalculateSum(int num1, int num2){ // parameters or formal parameters
+    int sum = num1+num2;
+    System.out.println("Sum is:" + sum);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        CalculateSum(a, b);  // arguments or actual parameters
+    }
+}
--- Functions/Function.java
+++ Functions/Function.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 public class Function {
     public static void printHelloWorld(){
