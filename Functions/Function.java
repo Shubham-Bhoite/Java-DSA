b4367dbacf2d50dae8c47c8cea78bc65ba41829c
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-08-24 22:11:38 +0530

    DAY 6

---
+++ Functions/Function.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Function {
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+
+    public static void main(String args[]){
+        printHelloWorld();
+    }
+}
