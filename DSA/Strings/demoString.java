9f98d9a3721573aded10c1c96b65bae1b2ca740c
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-08-31 19:52:43 +0530

    just started strings

---
+++ DSA/Strings/demoString.java
@@ -0,0 +1,13 @@
+// strings
+// Strings are Immutable(can not change)
+
+
+public class demoString{
+    public static void main(String args[]){
+        char arr[] = {'a', 'b', 'c', 'd', 'e'};
+        String str = "abcde";
+        String str2 = new String("xyz");
+
+        System.out.println(str2);
+    }
+}
