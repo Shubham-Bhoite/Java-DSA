9fb8ee4956b2d8e66aa9fa906ecdf00822546025
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-09-01 19:34:17 +0530

    updated string

---
+++ DSA/Strings/InputOutputString.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class InputOutputString {
+    public static void main(String args[]){
+    char arr[] = {'a', 'b', 'c', 'd', 'e'};
+        String str = "abcde";
+        String str2 = new String("xyz");
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println(name);
+}
+}
